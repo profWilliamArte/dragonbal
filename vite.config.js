4c06f3ea8feebe41b0dbacba2d91d13364ce5b12
arsistemadpw13 <arsistemadpw@gmail.com>
2025-05-16 13:00:12 -0400

    Primer commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-			base: "https://nombredetucuenta.github.io/dragonbal",
+			base: "https://profWilliamArte.github.io/dragonbal",
 		})
 
